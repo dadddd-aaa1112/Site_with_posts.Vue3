745859d6edc21c7a6976b5a6889d7e64866f6c73
Daria <you@example.com>
2022-02-10 14:39:07 +0300

    статический роутинг

---
+++ src/pages/Main.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="app">
+		<h1>About</h1>
+	</div>
+</template>
+<script>
+export default {}
+</script>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+.app {
+	padding: 20px;
+}
+</style>

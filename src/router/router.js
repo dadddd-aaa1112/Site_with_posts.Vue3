18710ccb43d7155630394c246c6d1816b804bbe9
Daria <you@example.com>
2022-02-11 19:26:55 +0300

    create store

--- src/router/router.js
+++ src/router/router.js
@@ -1,29 +1,36 @@
 import Main from '@/pages/Main'
-import About from '@/pages/About'
 import UserPage from '@/pages/UserPage'
+import About from '@/pages/About'
 import PostIdPage from '@/pages/PostIdPage'
+import PostPageWithStore from '@/pages/PostPageWithStore'
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+	{
+		path: '/',
+		component: Main,
+	},
+	{
+		path: '/users',
+		component: UserPage,
+	},
+	{
+		path: '/about',
+		component: About,
+	},
+	{
+		path: '/users/:id',
+		component: PostIdPage,
+	},
+	{
+		path: '/store',
+		component: PostPageWithStore,
+	},
+]
+
 const router = createRouter({
-	history: createWebHistory(),
-	routes: [
-		{
-			path: '/',
-			component: Main,
-		},
-		{
-			path: '/about',
-			component: About,
-		},
-		{
-			path: '/users',
-			component: UserPage,
-		},
-		{
-			path: '/users/:id',
-			component: PostIdPage,
-		},
-	],
+	routes,
+	history: createWebHistory(process.env.BASE_URL),
 })
 
 export default router
